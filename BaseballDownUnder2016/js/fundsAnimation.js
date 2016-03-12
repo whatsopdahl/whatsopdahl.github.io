5ebb28874aebca4b31eb28fbc1286b1fb8a78890
whatsopdahl <jopdahl12@gmail.com>
2016-03-11 20:40:56 -0600

    updated donor list and funding

--- BaseballDownUnder2016/js/fundsAnimation.js
+++ BaseballDownUnder2016/js/fundsAnimation.js
@@ -8,7 +8,7 @@ $( document ).ready(function(){
 	$bg = $('#fundsAnimation').parent();
 	var ctx = canvas.getContext("2d");
 	//variables dealing with funds
-	var raised = 600;
+	var raised = 700;
 	var percent = raised/5298;
 	var length = 373.162;
 	var destination = percent * length;
