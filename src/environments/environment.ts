fcd1d7e366fdebb745e5b3e099d46047c04fbc44
whatsopdahl <jopdahl12@gmail.com>
2017-11-05 19:56:51 -0600

    ass assets folder a root directory as temp fix for seving assets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,6 @@ export const environment = {
   googleAuth : {
     clientId : '428288460252-iupeh39m4803mdc70prmh63brlficjmp.apps.googleusercontent.com',
     clientSecret : 'tmqZKrLE7CqrsiJJDLyrjovy'
-  }
+  },
+  urlPrefix : ''
 };
