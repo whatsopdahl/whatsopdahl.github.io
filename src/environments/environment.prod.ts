8dee73a31e54b8695b397102a029c78c7ec11d98
whatsopdahl <jopdahl12@gmail.com>
2017-11-30 19:35:26 -0600

    rebuild to dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     clientId : '428288460252-iupeh39m4803mdc70prmh63brlficjmp.apps.googleusercontent.com',
     clientSecret : 'tmqZKrLE7CqrsiJJDLyrjovy'
   },
-  urlPrefix : 'whatsopdahl.com/'
+  urlPrefix : ''
 };
